7d2fc06d3b6cc3e8dc09b84a635c22a3ee677d40
CodeJeffreyG <99752992+CodeJeffreyG@users.noreply.github.com>
2025-01-04 22:59:59 -0500

    fixed direction

--- Assets/Scripts/Snake.cs
+++ Assets/Scripts/Snake.cs
@@ -1,55 +1,51 @@
 using System;
 using JetBrains.Annotations;
+using Unity.Mathematics;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class Snake : MonoBehaviour
 {
     [SerializeField] Transform snakeTransform;
-    Vector2 currentDirection;
-    public Vector2 right = Vector2.right;
-    public Vector2 left = Vector2.left;
-    public Vector2 down = Vector2.down;
-    public Vector2 up = Vector2.up;
+    Vector2 currentDirection = Vector2.right;
+
 
     public void Update()
     {
         if (Input.GetKeyDown(KeyCode.W))
         {
             Debug.Log("Up is pressed");
-            currentDirection = up;
+            currentDirection = Vector2.up;
         }
 
         else if (Input.GetKeyDown(KeyCode.A))
         {
             Debug.Log("Left is pressed");
-            currentDirection = left;
+            currentDirection = Vector2.left;
         }
 
         else if (Input.GetKeyDown(KeyCode.S))
         {
             Debug.Log("Right is pressed");
-            currentDirection = down;
+            currentDirection = Vector2.down;
         }
 
         else if (Input.GetKeyDown(KeyCode.D))
         {
             Debug.Log("Down is pressed");
-            currentDirection = right;
+            currentDirection = Vector2.right;
         }
     }
 
     public void FixedUpdate()
     {
-        float currentX = snakeTransform.position.x;
-        float currentY = snakeTransform.position.y;
         snakeTransform.position = new Vector3(
-            Mathf.Round(currentX + currentDirection.x),
-            Mathf.Round(currentY + currentDirection.y),
+            Mathf.Round(snakeTransform.position.x) + currentDirection.x,
+            Mathf.Round(snakeTransform.position.y) + currentDirection.y,
             0
         );
 
-        Debug.Log(snakeTransform.position);
-        Debug.Log("Current direction: " + currentDirection);
+
 
     }
 
